d76a5def65153a298aacf08053598826b206b115
Rohit <28shettr@elmbrookstudents.org>
2026-09-12 18:48:11 -0500

    Config Updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/data/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/data/Config.java
@@ -2,22 +2,24 @@ package org.firstinspires.ftc.teamcode.data;
 
 public class Config {
     //Control Hub
-    public static int intakeMotor = 0;
-    public static int b = 1;
+    public static int frontLeftMotor = 0;
+    public static int backLeftMotor = 1;
     public static int c = 2;
-    public static int d = 3;
+    public static int intakeMotor = 3;
 
-    public static int e = 1;
+    public static int rampServo = 1;
     public static int f = 2;
     public static int g = 3;
     public static int h = 4;
 
+    public static int bucketSensor = 0;
+
 
     //Expansion hub
-    public static int i = 0;
-    public static int j = 1;
+    public static int frontRightMotor = 0;
+    public static int backRightMotor = 1;
     public static int k = 2;
-    public static int l = 3;
+    public static int putterMotor = 3;
 
     public static int m = 0;
 
