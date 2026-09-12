20a73cc779c6f420dacf02cf3c038923dd0ddea7
rohit <rohitsuhasshetty@gmail.com>
2026-09-11 22:38:37 -0500

    Adding config

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/data/Config.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.data;
+
+public class Config {
+    //Control Hub
+    public static int x = 0;
+    public static int b = 1;
+    public static int c = 2;
+    public static int d = 3;
+
+    public static int e = 1;
+    public static int f = 2;
+    public static int g = 3;
+    public static int h = 4;
+
+
+    //Expansion hub
+    public static int i = 0;
+    public static int j = 1;
+    public static int k = 2;
+    public static int l = 3;
+
+    public static int m = 0;
+
+
+
+}
